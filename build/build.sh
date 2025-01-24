d2ca638599313a81f08276992d866a89dccf829a
Peter Seibel <peter@gigamonkeys.com>
2025-01-24 15:32:35 -0800

    Tweaks to build script.

--- build/build.sh
+++ build/build.sh
@@ -3,13 +3,12 @@
 #
 # Build the scavenger hunt from a set of clue and scripts that hide them. We
 # build the puzzle backwards so the script for each step can decide how to hide
-# the next clue and then emit the clue for its step. This allows the scripts to
-# generate random data or whatever and then create the clue based on where it
-# hid the secret.
+# the next clue that leads to the next step and then emit the clue for its step.
+# This allows the scripts to generate random data in which to hide the next
+# secret and then create their own clue based on where it hid the secret.
 #
 
 set -euo pipefail
-shopt -s lastpipe
 
 dir=$(dirname "$0")
 
@@ -24,6 +23,8 @@ rm -rf puzzle
 mkdir -p puzzle
 cd puzzle
 
+echo -n "Building puzzle "
+
 for step in "${steps[@]}"; do
     # Generate a random secret number for this step of the treasure hunt.
     id=$(printf "%x" "$SRANDOM")
@@ -31,9 +32,9 @@ for step in "${steps[@]}"; do
     # Our secret is our secret number but the clue that leads to the next secret
     secret=$(printf "%s: %s" "$id" "$clue")
 
-    # Script hide the clue given to it and emit its own clue to be hidden by the
-    # next script to run which will be for the preceding step of the treasure
-    # hunt.
+    # Each step's script hides the clue given to it and then emits its own clue
+    # to be hidden by the next script to run which will be for the preceding
+    # step of the treasure hunt.
     clue=$("../$dir/$step" "$secret")
 
     # Stash the hash of our secret number as part of the puzzle to the progress
@@ -41,5 +42,10 @@ for step in "${steps[@]}"; do
     hash=$(shasum <<< "$id" | cut -c -40)
     echo "$hash" >> .hashes
 
-    printf "clue: %s; id: %s; hash: %s\n" "$clue" "$id" "$hash"
+    # Progress indicator
+    echo -n "."
 done
+
+echo " done."
+
+# Optionally delete build directory so player can't see how puzzle was built.
