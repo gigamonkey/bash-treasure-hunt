2f716702ce830b1d64e52b24c10bd861f8fa53ab
Peter Seibel <peter@gigamonkeys.com>
2025-01-25 09:53:43 -0800

    Copy solutions in both directions.

--- copy-solutions.sh
+++ copy-solutions.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-set -x
 
 i=0
 
 while read -r s; do
-    cp "solutions/$s.sh" "$(printf "clue-%03d.sh" "$i")"
+
+    solution=solutions/$s.sh
+    clue=$(printf "clue-%03d.sh" "$i")
+
+    if [[ -e "$solution" && (! -e "$clue" || "$solution" -nt "$clue") ]]; then
+        cp "$solution" "$clue"
+    elif [[ -e "$clue" && (! -e "$solution" || "$clue" -nt "$solution") ]]; then
+        cp "$clue" "$solution"
+    else
+        "Echo not sure which to copy of $solution and $clue"
+    fi
+
     (( i += 1 ))
+
 done < build/STEPS
