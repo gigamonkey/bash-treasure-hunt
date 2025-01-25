5494eccd33d932ba3748558daeee9fdcf1bb4551
Peter Seibel <peter@gigamonkeys.com>
2025-01-25 08:25:28 -0800

    Make hay look more like needle.

--- build/functions.sh
+++ build/functions.sh
@@ -13,6 +13,13 @@ function fake_file_clue() {
     printf "The next secret is in %x.txt" "$SRANDOM"
 }
 
+# Take a real clue and replace the id with a new random number
+function fake_id() {
+    clue="$1"
+    secretish "${clue#*: }"
+}
+
+
 function random_file_in() {
     find "$1"  -maxdepth 1 -type f | shuf | head -1
 }
--- build/haystack.sh
+++ build/haystack.sh
@@ -9,7 +9,7 @@ mkdir -p haystack
 
 for f in haystack/{a..g}{a..g}.txt; do
     echo "just hay here" > "$f"
-    secretish "$(fake_file_clue)" >> "$f"
+    printf "%s\n" "$(fake_id "$1")" >> "$f"
 done
 
 echo "$1" > "$(random_file_in haystack)"
