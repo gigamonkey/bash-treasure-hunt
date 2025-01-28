8a1d65a49f63521ea626f4d5e70ae465eebc927d
Peter Seibel <peter@gigamonkeys.com>
2025-01-27 20:14:03 -0800

    Fix shellcheck thing.

--- build/check-hints.sh
+++ build/check-hints.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-cat STEPS | while read -r step; do
+while read -r step; do
     if [[ ! -e "$step.sh" ]]; then
         echo "ERROR: no script for $step"
     fi
     if [[ ! -e "$step.txt" ]]; then
         echo "WARNING: no hint for $step"
     fi
-done
+done < STEPS
