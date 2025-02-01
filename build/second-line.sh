e94e68d5ff52a350fe0a534e7e14df09ea5ce1be
ඞඞඞඞඞඞඞඞඞ <119468402+antidisestablishmentarianisms@users.noreply.github.com>
2025-02-01 19:34:04 +0000

    I tried

---
+++ build/second-line.sh
@@ -0,0 +1,11 @@
+touch me.txt
+var=me.txt
+echo "$1" > $var
+echo "asdfasdf /n this is the secret /n asdfasdf /m something something /n penis" >> $var
+
+echo The secret is in the second line of $var
+
+
+#I could not get this work because I had a "No puzzle directory!" error. 
+#I don't know how to get the puzzle to actually be created, because 
+#`./start` didn't work. I can edit it later if I get that figured out.
