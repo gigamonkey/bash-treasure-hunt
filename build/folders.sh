ba9e575adf7ffc094ebf364a29619a8e0dfa5005
DingoesDidIt <59072067+DigoesDidIt@users.noreply.github.com>
2025-02-03 22:47:53 +0000

    Added a folder puzzle

---
+++ build/folders.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Save the secret we are given into a super secret 
+start="${str:0:1}"
+str="$1"
+for ((i = 0; i < ${#str}; i++)); do
+    char="${str:i:1}"
+    mkdir "$char"
+    cd "$char" || exit 1
+done
+
+# Print the clue that will be given to the user.
+echo "The next secret is spelled out in the names of directories starting with /$start"
