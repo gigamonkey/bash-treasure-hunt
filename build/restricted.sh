46a20373170cf7c22ba23aa23c2d6ae88db8a8b2
emillaurimaa <142450503+emillaurimaa@users.noreply.github.com>
2025-02-04 01:33:51 +0000

    better clue ig

--- build/restricted.sh
+++ build/restricted.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 echo "$1" > locked.txt
 
-echo "Secret is in $PUZZLE/locked.txt. Can you decode it?"
+echo "Secret is in $PUZZLE/locked.txt. You're not allowed to open it though."
 
 chmod 000 locked.txt
