4a79a14a2bf61cfefa17a7bc1edea83ce122a945
sirineelkaroui <selkaroui1@gmail.com>
2025-02-07 22:29:12 +0000

    populating the files

--- build/x-marks-the-spot.sh
+++ build/x-marks-the-spot.sh
@@ -5,25 +5,29 @@ set -euo pipefail
 touch map.txt
 touch treasure.txt
 
-#num lines: 100 
+len=100 #number of lines
+loc="$RANDOM" % len #location of the x
+#echo "$loc"
 
-loc = "$RANDOM" % 100
-echo "$loc"
+#in ascii, 97-122 (inclusive) are all lowercase letters
+#x = 120
+function random_lowercase_string(){
+    line_length="$1"
+    for 
+}
 
-for line in 100; do
-    if [[ $line == loc ]]; then
+for line in {0.."$len"}; do
+    if [[ "$line"=="$loc" ]]; then
         #add a ton of letters and x
+        #add secret to treasure
+       "$1" >> $PUZZLE/treasure.txt
     fi
     else
     #add a random jumble of letters
+    #add secretish to treasure
+        secretish >> $PUZZLE/treasure.txt
+        #DOES SECRETISH NEED ARGS? IT LOOKS LIKE IT BUT I DON'T KNOW WHICH ONE TO PUT IN
 done
-
-for line in 100; do
-    if [[ $line == loc ]]; then
-       #add secret to treasure
-    fi
-    else #add secretish to treasure
-done 
 
 echo "The secret is hidden in the same line number of the $PUZZLE/treasure.txt 
 that the letter x occurs in the file $PUZZLE/map.txt"
