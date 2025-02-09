dc15518f2b0b11e539851d51559949894616b8ee
sirineelkaroui <selkaroui1@gmail.com>
2025-02-09 05:11:14 +0000

    working, going to replace echo with printf

--- build/x-marks-the-spot.sh
+++ build/x-marks-the-spot.sh
@@ -2,19 +2,17 @@
 
 set -euo pipefail
 
+#make the files and directories to store the map and treasure
 PIRATE=pirate
 mkdir pirate
-
 touch "$PIRATE"/map.txt
 touch "$PIRATE"/treasure.txt
 
-len=100 #number of lines
-row_len=100
+len=100 #length of file
+row_len=100 #length of row
 loc=$(("$RANDOM"%len)) #location of the x
-#echo "$loc"
 
-#in ascii, 97-122 (inclusive) are all lowercase letters
-#x = 120
+#in ascii, 97-122 (inclusive) are all lowercase letters, x = 120
 function random_lowercase_string(){
     string=""
     for ((i=1; i<="row_len"; i++)); do
@@ -22,15 +20,14 @@ function random_lowercase_string(){
         while [[ $num == 120 ]]; do
             num=$(("$RANDOM" % 26 + 97))
         done
-        #ascii not working
+        #add the number, converted to an ascii letter, to the string
         string+=$(printf "\\$(printf '%03o' "$num")")
     done
     echo "$string"
 }
-echo "$loc"
 for ((line=1; line <= "$len"; line++)); do
     if [[ "$line" == "$loc" ]]; then
-        #add a ton of letters and x, working
+        #make a jumble of letters, then cut it to put in x at a random position
         string=$(random_lowercase_string "(($row_len-1))")
         position=$(("$RANDOM" % "$row_len"))
         beg_string=$(echo "$string" | cut -b -"$position")
@@ -40,13 +37,12 @@ for ((line=1; line <= "$len"; line++)); do
         #add secret to treasure
         echo "$1" >> "$PIRATE"/treasure.txt
     else
-        #add a random jumble of letters, working
+        #add a random jumble of letters
         random_lowercase_string "$row_len" >> "$PIRATE"/map.txt
         #add secretish to treasure, doesn't recognize the function
         echo fake_secret >> "$PIRATE"/treasure.txt
         #$(fake_id "$1") >> "$PIRATE"/treasure.txt
     fi
 done
-
 
 echo "The secret is hidden in the same line number of the $PIRATE/treasure.txt that the letter x occurs in the file $PIRATE/map.txt"
