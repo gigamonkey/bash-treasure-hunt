5f2d52e3991b4641444f32f9ab0731a6f4987eac
jacobmccaughrin <142435737+jacobmccaughrin@users.noreply.github.com>
2025-02-10 07:52:04 +0000

    made some changes the part creation of the files, and fixed some syntax with brakets

--- build/sort-dir.sh
+++ build/sort-dir.sh
@@ -6,34 +6,38 @@ FOLDER="sortDirCOlor"
 mkdir -p "$FOLDER"
 SECRET=$1
 
-#split the secret into 7 parts
 part_length=$(( ${#SECRET} / 7 ))
-parts=()
-for ((i= 0; i < 7; i++)); do
-    parts[$i]="${SECRET:i*part_length:part_length}"
-done
+remainder=$(( ${#SECRET} % 7 ))
 
+declare -a parts=()
+for ((i = 0; i < 7; i++)); do
+    if (( i == 6 )); then
+        parts[$i]="${SECRET:$((i * part_length))}" 
+    else
+        parts[$i]="${SECRET:$((i * part_length)):part_length}"
+    fi
+done
 
 rainbow_colors=("red" "orange" "yellow" "green" "blue" "indigo" "violet") #sets our colors  
 
-total_files=$(((RANDOM % 100 + 1) * 7 )) #picks a random amount of files 
+total_files=$(( (RANDOM % 100 + 1) * 7 )) #picks a random amount of files 
 
-files_per_color=$((TOTAL_FILES / 7 )) #how many files per each color 
+files_per_color=$((total_files / 7 )) #how many files per each color 
 
 count=0
 
 for color in "${rainbow_colors[@]}"; do #for every color 
     first_file_created=false
     for ((i= 0; i<files_per_color; i++)); do #for every file per color 
-        if [[ $first_file_created == false]]; then
+        if [[ $first_file_created == false ]]; then
             FILENAME="${color}_${parts[$count]}.txt"
             touch "$FOLDER/$FILENAME"
             first_file_created=true
-            count+=1
+            ((count++))
         else
         
             rand_str=$(tr -dc 'a-z0-9' </dev/urandom | head -c 5) #generate random string
-            rand_num=$((RANDOM % 3)) #pick a number 0 - 2 
+            rand_num=$(( RANDOM % 3 )) #pick a number 0 - 2 
 
             if [[ $rand_num -eq 0 ]]; then #if 0 color at the start 
                 FILENAME="${color}_${rand_str}.txt"
