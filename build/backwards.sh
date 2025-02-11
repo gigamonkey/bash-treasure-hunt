b6836ac2afe5b4574beb41e99b2b488390ac72f3
StevenDen <119467873+TheHerders@users.noreply.github.com>
2025-02-11 23:25:08 +0000

    Removing previous backwards files

--- build/backwards.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-echo "$1" | rev > backwards.txt
-
-echo "Secret is in $PUZZLE/backwards.txt. Can you decode it?"
-
--- solutions/backwards.sh
+++
@@ -1,5 +0,0 @@
-set -euo pipefail
-
-# Secret is in puzzle/backwards.txt. Can you decode it?
-
-rev puzzle/backwards.txt
