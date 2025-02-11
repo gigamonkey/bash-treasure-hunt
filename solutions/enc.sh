873924eabb7444e15ecec17f22e223e5d1bd887f
Orion Kanat <58835564+okcoder2019@users.noreply.github.com>
2025-02-11 23:26:32 +0000

    One last change

--- solutions/enc.sh
+++ solutions/enc.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 key=tr 'a-zA-Z' 'n-za-mN-ZA-M' < puzzle/password
 
-openssl enc -aes-256-cbc -d -a -salt -pass pass:"$key" -in encrypted.txt
+openssl enc -aes-256-cbc -d -a -salt -pass $key -in encrypted.txt
