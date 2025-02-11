a4233364bd31c6a63f4fb65fd4e357103fa8c285
luke <111320483+lashlockbhs@users.noreply.github.com>
2025-02-11 23:19:04 +0000

    done

--- build/last-column.sh
+++ build/last-column.sh
@@ -3,11 +3,15 @@
 set -euo pipefail
 
 FILE=columns2.txt
+INPUT="$1"
 
-for ((i = 1; i <= ${#$1}; i++)); do
+touch $FILE
+for ((i = 1; i <= 40; i++)); do
     tmp=$(mktemp)
-    paste -d '' "$FILE" <(printf "%s\n" "$(fake_id "$1" | fold -w1)$("$1" | fold -w1)") > "$tmp" #adds 1 character of secret to end of line 
+    paste -d '' "$FILE" <(printf "%s\n" "$(fake_id "$1" | fold -w1)") > "$tmp"
     mv "$tmp" "$FILE"
 done
 
-echo echo "Secret is in the last column of $PUZZLE/$FILE"
+paste -d '' "$FILE" <(echo "$INPUT" | fold -w1) > "$tmp"
+
+echo "Secret is in the last column of $PUZZLE/$FILE"
