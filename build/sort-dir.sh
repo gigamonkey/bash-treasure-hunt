a638af29d88fcfa195637055e939755f8a63a984
jacobmccaughrin <142435737+jacobmccaughrin@users.noreply.github.com>
2025-02-11 23:31:51 +0000

    made some small changes and started to fix the errors after getting it to a runable state

--- build/sort-dir.sh
+++ build/sort-dir.sh
@@ -7,17 +7,19 @@ mkdir -p "$FOLDER"
 SECRET=$1
 
 part_length=$(( ${#SECRET} / 7 ))
-remainder=$(( ${#SECRET} % 7 ))
 
-declare -a parts=()
-for ((i = 0; i < 7; i++)); do
-    if (( i == 6 )); then
-        parts[$i]="${SECRET:$((i * part_length))}" 
-    else
-        parts[$i]="${SECRET:$((i * part_length)):part_length}"
-    fi
+# Create an array to hold the parts
+for i in {0..6}; do
+  start=$((i * part_length))
+  if [ $i -eq 6 ]; then
+    # For the last part, take the rest of the string
+    part="${SECRET:start}"
+  else
+    part="${SECRET:start:part_length}"
+  fi
+  array[$i]="$part"
 done
-
+ 
 rainbow_colors=("red" "orange" "yellow" "green" "blue" "indigo" "violet") #sets our colors  
 
 total_files=$(( (RANDOM % 100 + 1) * 7 )) #picks a random amount of files 
@@ -25,13 +27,15 @@ total_files=$(( (RANDOM % 100 + 1) * 7 )) #picks a random amount of files
 files_per_color=$((total_files / 7 )) #how many files per each color 
 
 count=0
-
+declare -a real_files={}
 for color in "${rainbow_colors[@]}"; do #for every color 
     first_file_created=false
     for ((i= 0; i<files_per_color; i++)); do #for every file per color 
         if [[ $first_file_created == false ]]; then
-            FILENAME="${color}_${parts[$count]}.txt"
+            FILENAME="${color}_${array[$count]}.txt"
             touch "$FOLDER/$FILENAME"
+            echo "$FOLDER/$FILENAME"
+            echo "$file_num"
             first_file_created=true
             ((count += 1))
         else
@@ -44,16 +48,21 @@ for color in "${rainbow_colors[@]}"; do #for every color
             elif [[ $rand_num -eq 1 ]]; then #if 1 color at the end
                 FILENAME="${rand_str}_${color}.txt"
             else # if 2 color in the middle 
-                rand_str1=$(tr -dc 'a-z0-9' </dev/urandom | head -c 5) #if the color ends up being in the middle we need another random string 
+                rand_str1=$(tr -dc 'a-z0-9' </dev/urandom | head -c 5) || true #if the color ends up being in the middle we need another random string 
                 FILENAME="${rand_str}_${color}_${rand_str1}.txt"
             fi
         
             touch "$FOLDER/$FILENAME" #create the file in the directory 
         fi
     done
+
 done
 
 
-echo $PUZZLE/$FOLDER is a directory with some number of files. Each file has in its name one of the colors of the rainbow: red, orange, yellow, green, blue, indigo, and violet
+#echo $PUZZLE/$FOLDER is a directory with some number of files. Each file has in its name one of the colors of the rainbow: red, orange, yellow, green, blue, indigo, and violet
 
 echo "The secret is found by joining the names of the first file found for each color in the rainbow."
+
+for i in {0..6}; do
+  echo "Part $((i+1)): ${array[$i]}"
+done
