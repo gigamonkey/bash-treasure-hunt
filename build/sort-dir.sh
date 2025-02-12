cca4e0450faacba4a9938b5236e3f2da5d20d8eb
jacobmccaughrin <142435737+jacobmccaughrin@users.noreply.github.com>
2025-02-12 21:25:19 +0000

    made simplier will fix the bugs later

--- build/sort-dir.sh
+++ build/sort-dir.sh
@@ -22,40 +22,13 @@ done
  
 rainbow_colors=("red" "orange" "yellow" "green" "blue" "indigo" "violet") #sets our colors  
 
-total_files=$(( (RANDOM % 100 + 1) * 7 )) #picks a random amount of files 
-
-files_per_color=$((total_files / 7 )) #how many files per each color 
-
-count=0
-declare -a real_files={}
+part_count=0
 for color in "${rainbow_colors[@]}"; do #for every color 
-    first_file_created=false
-    for ((i= 0; i<files_per_color; i++)); do #for every file per color 
-        if [[ $first_file_created == false ]]; then
-            FILENAME="${color}_${array[$count]}.txt"
-            touch "$FOLDER/$FILENAME"
-            echo "$FOLDER/$FILENAME"
-            echo "$file_num"
-            first_file_created=true
-            ((count += 1))
-        else
-        
-            rand_str=$(tr -dc 'a-z0-9' </dev/urandom | head -c 5) || true #generate random string
-            rand_num=$(( RANDOM % 3 )) #pick a number 0 - 2 
 
-            if [[ $rand_num -eq 0 ]]; then #if 0 color at the start 
-                FILENAME="${color}_${rand_str}.txt"
-            elif [[ $rand_num -eq 1 ]]; then #if 1 color at the end
-                FILENAME="${rand_str}_${color}.txt"
-            else # if 2 color in the middle 
-                rand_str1=$(tr -dc 'a-z0-9' </dev/urandom | head -c 5) || true #if the color ends up being in the middle we need another random string 
-                FILENAME="${rand_str}_${color}_${rand_str1}.txt"
-            fi
-        
-            touch "$FOLDER/$FILENAME" #create the file in the directory 
-        fi
-    done
-
+  rand_str=$(tr -dc 'a-z0-9' </dev/urandom | head -c 5) || true #generate random string
+  FILENAME="${color}_${rand_str}_${array["$part_count"]}.txt"
+  ((part_count++))
+  touch "$FOLDER/$FILENAME" #create the file in the directory 
 done
 
 
@@ -63,6 +36,4 @@ done
 
 echo "The secret is found by joining the names of the first file found for each color in the rainbow."
 
-for i in {0..6}; do
-  echo "Part $((i+1)): ${array[$i]}"
-done
+
