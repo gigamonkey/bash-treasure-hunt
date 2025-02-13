5b988b1a7d61ab696fe81530f0e0b9bd11649120
DingoesDidIt <59072067+DigoesDidIt@users.noreply.github.com>
2025-02-13 23:13:15 +0000

    final commit for real

--- solutions/folders64.sh
+++ solutions/folders64.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 # The next secret is encoded in the names of directories starting with /folders
-find puzzle/folders | tail -1 | tr -d "/" | cut -c 8- | base64 -d
+find puzzle/folders | tail -1 | tr -d "/" | cut -c 14- | base64 -d
