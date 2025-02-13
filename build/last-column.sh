1aa4bbc86fdc36e958bd723c5f3e077f363247e1
luke <111320483+lashlockbhs@users.noreply.github.com>
2025-02-13 22:54:09 +0000

    workds

--- build/last-column.sh
+++ build/last-column.sh
@@ -6,12 +6,16 @@ FILE=columns2.txt
 INPUT="$1"
 
 touch $FILE
-for ((i = 1; i <= 40; i++)); do
+for ((i = 0; i <= 40; i++)); do
     tmp=$(mktemp)
     paste -d '' "$FILE" <(printf "%s\n" "$(fake_id "$1" | fold -w1)") > "$tmp"
     mv "$tmp" "$FILE"
 done
+for ((i = 0; i <= ${#INPUT}; i++)); do
+    CHAR="${INPUT:$i:1}"
+    J=$((i+1))
+    sed -i "${J}s/$/ $CHAR/" "$FILE"
+done
 
-paste -d '' "$FILE" <(echo "$INPUT" | fold -w1) > "$tmp"
 
 echo "Secret is in the last column of $PUZZLE/$FILE"
