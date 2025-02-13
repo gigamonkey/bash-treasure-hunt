f64b747ac73be74f6caab2042bede555c3507267
DingoesDidIt <59072067+DigoesDidIt@users.noreply.github.com>
2025-02-13 22:53:57 +0000

    hopefully the last commit

--- build/folders64.sh
+++ build/folders64.sh
@@ -3,8 +3,10 @@
 # Save the secret we are given into a super secret 
 set -euo pipefail
 
-str="$(echo "$1" | base64)"
+str="$(echo "$1" | base64 -w 0)"
 start="${str:0:1}"
+mkdir folders
+cd folders
 for ((i = 0; i < ${#str}; i++)); do
     char="${str:i:1}"
     mkdir "$char"
@@ -12,4 +14,4 @@ for ((i = 0; i < ${#str}; i++)); do
 done
 
 # Print the clue that will be given to the user.
-echo "The next secret is encoded in the names of directories starting with /$start"
+echo "The next secret is encoded in the names of directories starting with /folders"
