15df39061ec3722a64f5aa56b29888c9eb31b94f
Peter Seibel <peter@gigamonkeys.com>
2026-02-16 11:30:22 -0800

    Put files at all levels in biggest-file.sh

--- build/biggest-file.sh
+++ build/biggest-file.sh
@@ -7,18 +7,25 @@ DIR=sizes
 biggest_size=0
 biggest_file=
 
-for f in $DIR/{foo,bar,baz}/{fred,wilma,barney,betty}/{a..g}.txt; do
-    mkdir -p "$(dirname "$f")"
-    size=$(( RANDOM % 256 ))
-    if [[ "$size" -ge "$biggest_size" ]]; then
-        if [[ "$size" -eq "$biggest_size" ]]; then
-            size=$((size + 1))
-        fi
-        biggest_size=$size
-        biggest_file=$f
-    fi
-    (yes || true) | head -n "$size" | tr -d '\n' > "$f"
+printf -v bigtext "x%.0s" {1..256}
+
+for d in $DIR/{foo,bar,baz}/{fred,wilma,barney,betty}; do
+    mkdir -p "$d"
 done
+
+while read -r -d '' d; do
+    for f in "$d/"{a..g}.txt; do
+        size=$(( RANDOM % 256 ))
+        if [[ "$size" -ge "$biggest_size" ]]; then
+            if [[ "$size" -eq "$biggest_size" ]]; then
+                size=$((size + 1))
+            fi
+            biggest_size=$size
+            biggest_file=$f
+        fi
+        echo -n "${bigtext:0:$size}" > "$f"
+    done
+done < <(find "$DIR" -type d -print0)
 
 echo -e "\n$1" >> "$biggest_file"
 
