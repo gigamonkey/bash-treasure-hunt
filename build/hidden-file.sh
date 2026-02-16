6aefda0a2081f5bd9b84f15f2d641d0dcd822c91
Peter Seibel <peter@gigamonkeys.com>
2026-02-16 10:19:43 -0800

    Fix spelling.

--- build/hidden-file.sh
+++ build/hidden-file.sh
@@ -3,18 +3,18 @@
 set -euo pipefail
 
 LINE=16
-FILE=menylines.txt
+FILE=manylines.txt
 
 for ((i = 1; i <= 15; i++)); do
     printf "%s\n" "$(fake_id "$1")" >> "$FILE"
 done
 
-incoded=$(echo "$1" | tr 'a-zA-Z' 'n-za-mN-ZA-M')
+encoded=$(echo "$1" | tr 'a-zA-Z' 'n-za-mN-ZA-M')
 
-echo "$incoded" >> "$FILE"
+echo "$encoded" >> "$FILE"
 
 for ((i = 1; i <= 75; i++)); do
     printf "%s\n" "$(fake_id "$1")" >> "$FILE"
 done
 
-echo "Secret is line $LINE of the file $PUZZLE/$FILE" but it may be coded
+echo "Secret is line $LINE of the file $PUZZLE/$FILE but it may be coded"
