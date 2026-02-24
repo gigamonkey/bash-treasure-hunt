a7980a825754f578356f5315c106ce473782b3bf
IsaacJurnove <isaacjurnove@students.berkeley.net>
2026-02-24 19:40:00 +0000

    b

--- build/repeating-lines.sh
+++ build/repeating-lines.sh
@@ -11,11 +11,11 @@ ensure_lines "$FILE" 12
 
 # random_fake_lines "$FILE" 12 "$1"
 
-for ((i = 0; i <= 11; i++)); do
-    line=$(head -"$i" | tail -1)
+for ((i = 1; i <= 12; i++)); do
+    line=$(head -"$i" "$FILE" | tail -1)
 
-    for ((j = -1; j <= "$i"; j++)); do
-        echo "$line" >> "$FILE"
+    for ((j = 0; j <= "$i"; j++)); do
+        echo "$line" >> $FILE
     done
 done
 
